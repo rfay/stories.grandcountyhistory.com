3787fc42cd270a20956785c8d2a56bdb50b8c66e
Randy Fay <randy@randyfay.com>
2014-10-15 17:21:43 -0600

    colorbox fixup

--- sites/all/modules/colorbox/styles/default/colorbox_default_style.js
+++ sites/all/modules/colorbox/styles/default/colorbox_default_style.js
@@ -2,9 +2,10 @@
 
 Drupal.behaviors.initColorboxDefaultStyle = {
   attach: function (context, settings) {
-    $(context).bind('cbox_complete', function () {
+    $(document).bind('cbox_complete', function () {
       // Only run if there is a title.
       if ($('#cboxTitle:empty', context).length == false) {
+        setTimeout(function () { $('#cboxTitle', context).slideUp() }, 1500);
         $('#cboxLoadedContent img', context).bind('mouseover', function () {
           $('#cboxTitle', context).slideDown();
         });
