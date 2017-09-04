abacc458fe94735163be250307fb8d357f5b5460
Randy Fay <randy@randyfay.com>
2017-09-04 14:55:17 -0600

    Upgrade colorbox  Name                 Installed Version  Proposed version  Message  Colorbox (colorbox)  7.x-1.6+0-dev      7.x-2.13          SECURITY UPDATE                                                            available

--- docroot/sites/all/modules/colorbox/styles/default/colorbox_style.js
+++ docroot/sites/all/modules/colorbox/styles/default/colorbox_style.js
@@ -1,11 +1,15 @@
+/**
+ * @file
+ * Colorbox module style js.
+ */
+
 (function ($) {
 
 Drupal.behaviors.initColorboxDefaultStyle = {
   attach: function (context, settings) {
-    $(document).bind('cbox_complete', function () {
+    $(context).bind('cbox_complete', function () {
       // Only run if there is a title.
       if ($('#cboxTitle:empty', context).length == false) {
-        setTimeout(function () { $('#cboxTitle', context).slideUp() }, 1500);
         $('#cboxLoadedContent img', context).bind('mouseover', function () {
           $('#cboxTitle', context).slideDown();
         });
